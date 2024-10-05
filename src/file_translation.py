05abfc2de84b515a5fae277af36864a49c0532f0
ilya <ilyavas@uw.edu>
2024-10-05 15:10:57 -0700

    start file

---
+++ src/file_translation.py
@@ -0,0 +1,9 @@
+import networkx as nx
+print("HElo")
+DG = nx.DiGraph()
+DG.add_edge(2, 1)   # adds the nodes in order 2, 1
+DG.add_edge(1, 3)
+DG.add_edge(2, 4)
+DG.add_edge(1, 2)
+assert list(DG.successors(2)) == [1, 4]
+assert list(DG.edges) == [(2, 1), (2, 4), (1, 3), (1, 2)]
