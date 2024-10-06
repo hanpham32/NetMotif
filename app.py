df0a99243f34e071acdea890b59d9bb67d97e66a
Han <phamhan@uw.edu>
2024-10-06 15:06:11 -0700

    Streamlit: Add graph options and visualization
    - Add options for Directed or Undirected
    - Visualize graph (but not yet interactive)

--- app.py
+++ app.py
@@ -1,20 +1,58 @@
 import streamlit as st
-from src.graph_utils import graph_generation
+from src.graph_utils import Graph
+
+
+def form_callback():
+    """
+    Handle form validation logic
+    """
+
+    if st.session_state['uploaded_file'] is None:
+        st.warning("Please upload a file.")
+    elif st.session_state['graph_type'] is None:
+        st.warning("Please select a graph type.")
+    else:
+        st.write("Succesfully uploaded file.")
+        G = Graph()
+        G.generate_graph(file=st.session_state['uploaded_file'], graph_type=st.session_state['graph_type'])
+
+        graph_properties = G.graph_properties()
+        st.write(f"Number of nodes: {graph_properties['Number of nodes']}")
+        # st.write(f"Edges: {graph_properties['Edges']}")
+        st.write(f"Number of edges: {graph_properties['Number of edges']}")
+        st.write(f"Weight: {graph_properties['Weight']}")
+
+        G.draw()
 
 
 def main():
-    st.write("NOTE: Uploading more than 1000 nodes might cause delay. Will implement loading progress to display status")
-    uploaded_file = st.file_uploader("Choose a file")
+    # Initialize global session state for user form submission
+    if 'graph_type' not in st.session_state:
+        st.session_state['graph_type'] = None
+    if 'uploaded_file' not in st.session_state:
+        st.session_state['uploaded_file'] = None
 
-    if uploaded_file is not None:
-        st.write("Succesfully uploaded file")
-        G = graph_generation(uploaded_file)
-        st.write(f"Number of nodes: {G.number_of_nodes()}")
-        st.write(f"Edges: {G.edges()}")
-        st.write(f"Number of edges: {G.number_of_edges()}")
-        st.write(f"Weight: {G.size()}")
-    else:
-        st.write("Upload a file to read the graph.")
+    with st.form(key="form"):
+        col1, col2 = st.columns([1, 3])
+        with col1:
+            graph_type = st.radio(
+                "Set Graph type:",
+                key="graph",
+                index=None,
+                options=["Directed", "Undirected"],
+            )
+        with col2:
+            uploaded_file = st.file_uploader("Choose a file")
+            st.write(
+                "NOTE: Uploading more than 1000 nodes might cause delay. Will implement loading progress to display status"
+            )
+
+        submitted = st.form_submit_button(label="Submit")
+
+    if submitted:
+        st.session_state['graph_type'] = graph_type
+        st.session_state['uploaded_file'] = uploaded_file
+        form_callback()
 
 
 if __name__ == "__main__":
