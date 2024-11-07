0fdad62fb844bea22929efbc8317ec14108b6fb2
ilya <ilyavas@uw.edu>
2024-11-07 13:40:33 -0800

    sample probabilities first implementation

--- src/esu.py
+++ src/esu.py
@@ -8,23 +8,27 @@ The class can identify subgraphs of specified size and return them for further\
 """
 
 import networkx as nx
+import random
 
 
 class ESU:
     def __init__(self, G: nx.Graph):
         self.G = G
 
-    def enumerate_subgraphs(self, size: int):
+    #currently does not allow for motif size over 10 because of default sample probability. should be fixed
+    def enumerate_subgraphs(self, size: int, sample_probability: list[float] = [1,1,1,1,1,1,1,1,1,1]):
         """
         Enumerates all unique subgraphs of a given motif size from the input\
                 graph using the ESU algorithm.
-        Translated for networkx from https://github.com/IlyaVasUW/NEMO_SUITE/
+        Translated and improved for networkx from https://github.com/IlyaVasUW/NEMO_SUITE/
         """
         subgraph_list = []
         nodes = self.G.nodes()
         node_visited = set()
 
-        for node in nodes:
+        sample_neighbors = self.eliminate_percentage(nodes, sample_probability[0])
+
+        for node in sample_neighbors:
             neighbor_set = set(self.get_right_neighbors(node))
             node_list = [node]
             node_visited.add(node)
@@ -32,7 +36,7 @@ class ESU:
 
         return subgraph_list
 
-    def esu_recursive_helper(self, size: int, neighbors: set, node_list: list, subgraph_list: list, nodes_visited: set):
+    def esu_recursive_helper(self, size: int, neighbors: set, node_list: list, subgraph_list: list, nodes_visited: set,  sample_probability: list[float]):
         if size == 1:
             subgraph_list.append(self.G.subgraph(node_list.copy()))
             return
@@ -40,12 +44,14 @@ class ESU:
         if len(neighbors) == 0:
             return
 
-        for node in neighbors:
+        sample_neighbors = self.eliminate_percentage(neighbors, sample_probability[len(node_list)])
+
+        for node in sample_neighbors:
             node_list.append(node)
             nodes_visited.add(node)
             next_neighbors = set()
 
-            for neighbor in neighbors:
+            for neighbor in sample_neighbors:
                 if neighbor not in nodes_visited:
                     next_neighbors.add(neighbor)
             for neighbor in self.G.neighbors(node):
@@ -70,3 +76,15 @@ class ESU:
                 right_hand_neighbors.append(n)
 
         return iter(right_hand_neighbors)
+
+    #Removes a given percentage of elements from a list randomly.
+    def eliminate_percentage(set: set, probability: float):
+        num_to_remove = int(len(set) * probability)
+        indices_to_remove = random.sample(range(len(set)), num_to_remove)
+
+        # Remove elements in reverse order to avoid index issues
+        for i in sorted(indices_to_remove, reverse=True):
+            set.pop(i)
+
+        return set
+
