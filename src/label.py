3ba20e5753beb5b066b660c14377409d3f4ead4b
IlyaVasUW <ilyavas@uw.edu>
2025-02-01 21:49:17 -0800

    testing

--- src/label.py
+++ src/label.py
@@ -98,13 +98,13 @@ def digraph6(graph: nx.DiGraph) -> str:
     return chr(38) + N + R
 
 def toLabelg(label:str):
-    label_g = "/labelg"  # Name of the executable
+    label_g = "../labelg"  # Name of the executable
     # Check if the labelg executable exists in the root directory
     if os.path.isfile(label_g):
         os.chmod(label_g, 0o755)  # Ensure it is executable
     else:
         st.write("labelg exists: False")
-        return
+        return label
 
     try:
         result = subprocess.run(
