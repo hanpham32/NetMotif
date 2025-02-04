f1c181fe2678f84f830fc89c8e5e944ba2d20ba7
ilya <ilyavas@uw.edu>
2025-02-04 14:20:01 -0800

    test added

--- best_test.py
+++ best_test.py
@@ -5,12 +5,10 @@ import src.random_graph as rg
 import streamlit as st
 from io import BytesIO
 
-print("running labelG test without streamlit")
-basic_graph = Graph(GraphType.UNDIRECTED, './NetMotif/data/basicTest.txt', 3)
+print("running stats table test")
+basic_graph = Graph(GraphType.UNDIRECTED, './NetMotif/data/bestTest.txt', 3)
 print(basic_graph.subgraph_list_enumerated)
 random_graphs = rg.generate_random_graphs(basic_graph, 10)
 # make a table of data for each label
 table = stat.processStatistics(basic_graph, random_graphs)
 print(table)
-
-print("running stats table test")
