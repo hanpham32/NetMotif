2f914eaac2029d8580140033d055f7e5c1b6dad7
ilya <ilyavas@uw.edu>
2025-02-04 15:07:49 -0800

    test for directed graph

--- best_test.py
+++ best_test.py
@@ -6,7 +6,7 @@ import streamlit as st
 from io import BytesIO
 
 print("running stats table test")
-basic_graph = Graph(GraphType.UNDIRECTED, './NetMotif/data/bestTest.txt', 3)
+basic_graph = Graph(GraphType.DIRECTED, './NetMotif/data/bestTest.txt', 3)
 print(basic_graph.subgraph_list_enumerated)
 random_graphs = rg.generate_random_graphs(basic_graph, 10)
 # make a table of data for each label
