ea3c5383e62c7a1c4ebb57206f8436da21cc4539
ilya <ilyavas@uw.edu>
2025-02-06 20:10:55 -0800

    uncomment

--- src/motif_statistics.py
+++ src/motif_statistics.py
@@ -6,7 +6,6 @@ import streamlit as st
 #import streamlit.components.v1 as components
 
 def draw_statistics(label_table: dict):
-    '''
     motif_table: dict = {}
     st.write(label_table.keys())
     for key in label_table.keys():
@@ -14,7 +13,6 @@ def draw_statistics(label_table: dict):
         new_key += key.get_label()
         #new_key += components.html(key.draw_graph())
         motif_table[new_key] = label_table[key]
-    '''
     df = pd.DataFrame.from_dict(label_table, orient = 'index')
     st.table(df)
 
