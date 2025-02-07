27eb8e1756b552a83dac55df96794df446325718
ilya <ilyavas@uw.edu>
2025-02-06 20:05:29 -0800

    quick rename

--- src/random_graph.py
+++ src/random_graph.py
@@ -4,20 +4,20 @@ from src.graph_types import GraphType
 
 def generate_random_graphs(mimicked_graph: GraphWithSubgraph, number_of_graphs) -> list["GraphWithSubgraph"]:
     random_graphs: list["GraphWithSubgraph"] = []
-    for i in range(number_of_graphs):
+    for _ in range(number_of_graphs):
         if mimicked_graph.graph_type == GraphType.UNDIRECTED:
             degree_sequence = [d for _, d in mimicked_graph.G.degree()]
-            random_graph = nx.Graph(nx.configuration_model(degree_sequence))
+            random_nx_graph = nx.Graph(nx.configuration_model(degree_sequence))
         elif mimicked_graph.graph_type == GraphType.DIRECTED:
             in_degree_sequence = [d for _, d in mimicked_graph.G.in_degree()]
             out_degree_sequence = [d for _, d in mimicked_graph.G.out_degree()]
-            random_graph = nx.DiGraph(
+            random_nx_graph = nx.DiGraph(
                 nx.directed_configuration_model(
                     in_degree_sequence, out_degree_sequence
                 )
             )
         random_graph = GraphWithSubgraph(
-            input=random_graph,
+            input=random_nx_graph,
             graph_type=mimicked_graph.graph_type,
             motif_size=mimicked_graph.motif_size,
         )
