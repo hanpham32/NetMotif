a30246219306ca285c5ecb274881b53d5dc4864e
IlyaVasUW <ilyavas@uw.edu>
2025-02-08 19:36:02 -0800

    changed variable names to subgraph in motif stat

--- src/motif_statistics.py
+++ src/motif_statistics.py
@@ -5,39 +5,39 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 
-def draw_statistics(label_table: dict):
+def draw_statistics(subgraph_table: dict):
     '''
     motif_table: dict = {}
-    st.write(label_table.keys())
-    for key in label_table.keys():
+    st.write(subgraph_table.keys())
+    for key in subgraph_table.keys():
         new_key = ""
         new_key += key.get_label()
         #new_key += components.html(key.draw_graph())
-        motif_table[new_key] = label_table[key]
+        motif_table[new_key] = subgraph_table[key]
     '''
-    df = pd.DataFrame.from_dict(label_table, orient = 'index')
+    df = pd.DataFrame.from_dict(subgraph_table, orient = 'index')
     st.table(df)
 
-#returns a dictionary of all stastical information for each unique label in graphs
+#returns a dictionary of all stastical information for each unique subgraph in graphs
 def process_statistics(original_graph: GraphWithSubgraph, graphs: list[GraphWithSubgraph]) -> dict:
-    label_table: dict = {} #label -> [frequency, mean, sd, zscore, p-value]
-    _generate_empty_label_table(original_graph, label_table)
+    subgraph_table: dict = {} #subgraph -> [frequency, mean, sd, zscore, p-value]
+    _generate_empty_subgraph_table(original_graph, subgraph_table)
     total_number_of_subgraphs = sum(original_graph.subgraph_list_enumerated.values())
-    for label in label_table:
-        #frequency of label in original graph as a percent
-        label_table[label]['freq'] = (original_graph.subgraph_list_enumerated[label]/total_number_of_subgraphs)*100
-        #st.write(original_graph.subgraph_list_enumerated[label])
+    for subgraph in subgraph_table:
+        #frequency of subgraph in original graph as a percent
+        subgraph_table[subgraph]['freq'] = (original_graph.subgraph_list_enumerated[subgraph]/total_number_of_subgraphs)*100
+        #st.write(original_graph.subgraph_list_enumerated[subgraph])
         #st.write(total_number_of_subgraphs)
-        mean = _getLabelMean(label, graphs)
-        label_table[label]['mean'] = mean * 100 # % mean-frequency as a percent
-        st.write(label_table[label]['mean'])
-        sd = _getStandardDeviation(mean, label, graphs)
-        label_table[label]['sd'] = sd # standard deviation
-        z_score = _getZScore(sd, mean, label, original_graph)
-        label_table[label]['z-score'] = z_score # z-score
+        mean = _getsubgraphMean(subgraph, graphs)
+        subgraph_table[subgraph]['mean'] = mean * 100 # % mean-frequency as a percent
+        st.write(subgraph.get_label() + subgraph_table[subgraph]['mean'])
+        sd = _getStandardDeviation(mean, subgraph, graphs)
+        subgraph_table[subgraph]['sd'] = sd # standard deviation
+        z_score = _getZScore(sd, mean, subgraph, original_graph)
+        subgraph_table[subgraph]['z-score'] = z_score # z-score
         p_value = _getPValue(z_score)
-        label_table[label]['p-value'] = p_value # p-value
-    return label_table
+        subgraph_table[subgraph]['p-value'] = p_value # p-value
+    return subgraph_table
 
 
 '''
@@ -45,7 +45,7 @@ method written by ChatGPT
 finds every unique key in a list of dictionaries and sets them as the
 keys in second input dictionary
 '''
-def _generate_empty_label_table(graph: GraphWithSubgraph, label_table: dict):
+def _generate_empty_subgraph_table(graph: GraphWithSubgraph, subgraph_table: dict):
     # Create an empty set to store unique keys
     unique_keys = set()
 
@@ -53,14 +53,14 @@ def _generate_empty_label_table(graph: GraphWithSubgraph, label_table: dict):
     unique_keys.update(graph.subgraph_list_enumerated.keys())
 
     for key in unique_keys:
-        label_table[key] = {'freq': 0,'mean': 0, 'sd': 0, 'z-score': 0, 'p-value': 0}
+        subgraph_table[key] = {'freq': 0,'mean': 0, 'sd': 0, 'z-score': 0, 'p-value': 0}
 
-def _getLabelMean(label, graphs: list[GraphWithSubgraph]):
+def _getsubgraphMean(subgraph, graphs: list[GraphWithSubgraph]):
     graph_frequency = 0
     frequencys = 0
     for graph in graphs:
-        if label in graph.subgraph_list_enumerated:
-            graph_frequency = graph.subgraph_list_enumerated[label]
+        if subgraph in graph.subgraph_list_enumerated:
+            graph_frequency = graph.subgraph_list_enumerated[subgraph]
             total_number_of_subgraphs = sum(graph.subgraph_list_enumerated.values())
             frequencys += graph_frequency/total_number_of_subgraphs
             #st.write(graph_frequency)
@@ -68,21 +68,21 @@ def _getLabelMean(label, graphs: list[GraphWithSubgraph]):
             #st.write(frequencys)
     return frequencys/len(graphs)
 
-def _getStandardDeviation(mean, label, graphs: list[GraphWithSubgraph]):
+def _getStandardDeviation(mean, subgraph, graphs: list[GraphWithSubgraph]):
     variance = 0
     for graph in graphs:
-        if label in graph.subgraph_list_enumerated:
-            xi = graph.subgraph_list_enumerated[label]
+        if subgraph in graph.subgraph_list_enumerated:
+            xi = graph.subgraph_list_enumerated[subgraph]
             variance += (xi-mean)**2
         else:
             variance += mean**2
     variance = variance/(len(graphs))
     return variance**0.5
 
-def _getZScore(sd: float, mean: float, label, original_graph: GraphWithSubgraph):
+def _getZScore(sd: float, mean: float, subgraph, original_graph: GraphWithSubgraph):
     score = 0
-    if(label in original_graph.subgraph_list_enumerated):
-        score = original_graph.subgraph_list_enumerated[label]
+    if(subgraph in original_graph.subgraph_list_enumerated):
+        score = original_graph.subgraph_list_enumerated[subgraph]
     return (score - mean)/sd
 
 def _getPValue(zscore: float):
