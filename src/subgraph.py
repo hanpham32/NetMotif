b4a1f31f869dfec8b66271191002a10702c82629
IlyaVasUW <ilyavas@uw.edu>
2025-02-15 00:21:26 -0800

    try new optimized labeling

--- src/subgraph.py
+++ src/subgraph.py
@@ -1,4 +1,5 @@
 from src.graph_utils import Graph
+from src.graph_types import GraphType
 import src.label as lb
 import streamlit as st
 
@@ -6,11 +7,12 @@ import streamlit as st
 A graph of same size as motif. GraphWithSubgraphs --<*> Subgraphs
 '''
 class Subgraph(Graph):
-    label: str
+    label_conversion_map: dict = {}
 
     def __init__(self, graph_type, input):
         super().__init__(graph_type, input)  # Call the parent's __init__
-        self.label = lb.get_graph_label(self.G, self.graph_type)
+        self.label = ""
+        self.set_label()
 
     def __eq__(self, other):
         if isinstance(other, Subgraph):
@@ -22,3 +24,13 @@ class Subgraph(Graph):
 
     def get_label(self):
         return self.label
+    
+    def set_label(self):
+        if self.graph_type == GraphType.UNDIRECTED:
+            basic_label = lb.graph6(self.G)
+        elif self.graph_type == GraphType.DIRECTED:
+            basic_label = lb.digraph6(self.G)
+        if basic_label not in Subgraph.label_conversion_map:
+            Subgraph.label_conversion_map[basic_label] = lb.get_graph_label(self.G, self.graph_type)
+        self.label = Subgraph.label_conversion_map[basic_label]
+            
